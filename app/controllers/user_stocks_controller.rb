57fa82a37831425a0869b569d2c8305c902d49bb
Ubuntu <ubuntu@ip-172-31-24-11.us-east-2.compute.internal>
2020-06-29 15:01:37 +0000

    add functionality to remove tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
         flash[:notice] = "#{stock.name} has successfully been added to your portfolio."
         redirect_to portfolio_path
     end
+    
+    def destroy
+        stock = Stock.find(params[:id])
+        @foundStock = UserStock.where(stock_id: stock.id, user_id: current_user.id).first
+        @foundStock.destroy
+         flash[:notice] = "#{stock.name} was deleted from your portfolio."
+        redirect_to portfolio_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
  
  def count_stocks?
-    stocks.count <= 10
+    stocks.count >= 10
  end
  
  def available_stock?(ticker_sym)
